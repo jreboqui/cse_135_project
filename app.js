8343ac9a6423706394e33c7e73ee48cdbc3649b1
Junnel Reboquio <jreboqui@ucsd.edu>
2019-01-28 16:39:45 -0800

    Import config files and reports, also images for final submission

---
+++ app.js
@@ -0,0 +1,28 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+const hostname = '142.93.82.54';
+
+const port = 8083;
+
+const server = http.createServer((req, res) => {
+	
+	iif (req.url === '/') {
+        	res.writeHead(200, {'Content-Type': 'text/html'});
+        	fs.createReadStream('index.html').pipe(res);
+    	} else if (ext.test(req.url)) {
+        	fs.exists(path.join(__dirname, req.url), function (exists) {
+            	if (exists) {
+                	res.writeHead(200, {'Content-Type': 'text/html'});
+                	fs.createReadStream('index.html').pipe(res);
+            	} else {
+                	res.writeHead(404, {'Content-Type': 'text/html'});
+                	fs.createReadStream('custom_404.html').pipe(res);
+        	});
+    	} else {
+        	//  add a RESTful service
+    	}});
+
+server.listen(port, hostname, () => {
+	console.log(`Server running at http://${hostname}:${port}/`);
+});
